71e6f78d140be9d7c4128aef07dc9e1511066a8b
charlotte <charlotte.finance@etu.univ-lyon1.fr>
2021-11-28 23:35:07 +0100

    ⬆️ New dependencies to allow requests

--- src/main/java/com/example/cinemaBE/CinemaBeApplication.java
+++ src/main/java/com/example/cinemaBE/CinemaBeApplication.java
@@ -2,7 +2,13 @@ package com.example.cinemaBE;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @SpringBootApplication
 public class CinemaBeApplication {
@@ -10,5 +16,25 @@ public class CinemaBeApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CinemaBeApplication.class, args);
 	}
+
+}
+
+
+@EnableWebSecurity
+class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.cors().and();
+		http.csrf().disable();
+	}
+
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+		return source;
+	}
+
 
 }
